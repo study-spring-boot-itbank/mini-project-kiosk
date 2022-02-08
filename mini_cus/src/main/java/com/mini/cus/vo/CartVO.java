3a03693b3ccc29e627c405fc99e29e457f3b576e
yBean0101 <jeonyb0101@naver.com>
2022-02-08 21:19:43 +0900

    1.03

--- mini_cus/src/main/java/com/mini/cus/vo/CartVO.java
+++ mini_cus/src/main/java/com/mini/cus/vo/CartVO.java
@@ -1,12 +1,6 @@
 package com.mini.cus.vo;
 
-import java.util.List;
-import java.util.Objects;
-
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
 @Data
 public class CartVO {
